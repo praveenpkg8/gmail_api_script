504908b73350a0d63f0d05d49604d07a59a0abaf
praveen <praveenpkg8@gmail.com>
2020-01-15 01:05:18 +0530

    Refactored code

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from database import init_db
 
 from config import get_encrypted_credentials, get_access_token
 
@@ -7,10 +8,16 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 credentials = get_encrypted_credentials()
 
-service = get_access_token()
-print(MailService.get_message(service, "me", "16fa1889e1b083b5"))
+def main():
+    service = get_access_token()
+    MailService.update_mail_in_database(service, "me")
+    # print(MailService.get_message(service, "me", "16fa1889e1b083b5"))
+    print("mail service stored in database")
 
 
+if __name__ == "__main__":
+    init_db()
+    main()
 
 
 
