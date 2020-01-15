cca9349b0b9b9ccc11241ec204d9117c1898c50c
praveen <praveenpkg8@gmail.com>
2020-01-16 03:12:06 +0530

    refactored code base

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
 from database import init_db
+from config import get_access_token, load_input_data
 
-from config import get_encrypted_credentials, get_access_token
 
-from services.mail_service import MailService
+from services.predicate_service import PredicateService
+from services.action_service import ActionService
 
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
-credentials = get_encrypted_credentials()
-
-def main():
+def perform_task():
     service = get_access_token()
-    MailService.update_mail_in_database(service, "me")
-    # print(MailService.get_message(service, "me", "16fa1889e1b083b5"))
-    print("mail service stored in database")
+    input_data_list = load_input_data()
+    for input_data in input_data_list:
+        if input_data.get('rule') == "ALL":
+            mail = PredicateService.all_predicate(
+                config=input_data.get('config'),
+                details=input_data.get('details')
+            )
+            if not mail:
+                continue
+
+            ActionService.perform_action(
+                service=service,
+                user_id="me",
+                msg_id=mail.id,
+                action=input_data.get('action')
+            )
+
+        elif input_data.get('rule') == 'ANY':
+            mail = PredicateService.any_predicate(
+                config=input_data.get('config'),
+                details=input_data.get('details')
+            )
+            if not mail:
+                continue
+
+            ActionService.perform_action(
+                service=service,
+                user_id="me",
+                msg_id=mail.id,
+                action=input_data.get('action')
+            )
 
 
 if __name__ == "__main__":
     init_db()
-    main()
+    perform_task()
+
 
 
 
