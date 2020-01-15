52626ce4bbd3cd734b9183b7191771fcdd626f93
praveen <praveenpkg8@gmail.com>
2020-01-15 21:12:15 +0530

    Added filter metrics

--- models/mail.py
+++ models/mail.py
@@ -1,18 +1,16 @@
-from sqlalchemy import Column, String, DateTime
-
+from sqlalchemy import Column, String, DateTime, and_, or_
 
 from database import Base, session
 
 
 class Mail(Base):
-
     __tablename__ = "mail"
-    
+
     id = Column(String, primary_key=True)
     sender = Column(String)
     receiver = Column(String)
     subject = Column(String)
-    time_received = Column(String)
+    time_received = Column(DateTime)
 
     @staticmethod
     def create_mail(mail):
@@ -26,4 +24,38 @@ class Mail(Base):
         session.add(_mail)
         session.commit()
 
+    @staticmethod
+    def filter_by_all_metrics(
+            sender_mail,
+            subject,
+            time_received
 
+    ):
+        mail = session.query(Mail).filter(and_(
+            Mail.sender.like(sender_mail),
+            Mail.subject == subject,
+            Mail.time_received > time_received
+        )).one()
+        print(mail.subject)
+        return mail
+
+    @staticmethod
+    def filter_by_any_metrics(
+            sender_mail,
+            subject,
+            time_received
+    ):
+        mail = session.query(Mail).filter(
+            Mail.time_received > time_received, or_(
+                Mail.sender.like(sender_mail),
+                Mail.subject == subject,
+            )).one()
+        print(mail.subject)
+        return mail
+
+    @staticmethod
+    def get_labels_by_id(
+            id
+    ):
+        mail = session.query(Mail).get(id)
+        return mail.labels
