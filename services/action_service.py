778b03ef748f4b4638f9236b5283cce3562c1931
praveen <praveenpkg8@gmail.com>
2020-01-16 03:07:32 +0530

    added parse action method

--- services/action_service.py
+++ services/action_service.py
@@ -2,19 +2,25 @@ from apiclient import errors
 
 
 from services.label_service import LabelService
+
+
 class ActionService:
 
     @classmethod
-    def perform_action(cls, service, user_id, msg_id, labels, action="READ"):
+    def perform_action(cls, service, user_id, msg_id, action):
 
+        label_name = action.get('label_name')
+        action = ActionService.parse_action(action)
         try:
             msg_labels = {'removeLabelIds': [], 'addLabelIds': []}
             if action == "READ":
                 msg_labels = ActionService.mark_as_read()
             elif action == "UNREAD":
                 msg_labels = ActionService.mark_as_unread()
+            elif action == "ARCHIVE":
+                msg_labels = ActionService.archive_mail()
             elif action == "LABELS":
-                msg_labels = ActionService.add_labels(service, labels)
+                msg_labels = ActionService.add_labels(service, label_name)
 
             message = service.users().messages().modify(userId=user_id, id=msg_id,
                                                         body=msg_labels).execute()
@@ -48,5 +54,22 @@ class ActionService:
     @classmethod
     def mark_as_unread(cls):
         return {'removeLabelIds': [], 'addLabelIds': ['UNREAD']}
+
+    @classmethod
+    def archive_mail(cls):
+        return {'removeLabelIds': ['INBOX'], 'addLabelIds': []}
+
+    @classmethod
+    def parse_action(cls, action):
+        if not action:
+            return "READ"
+        if action.get('mark_as_read'):
+            return "READ"
+        elif action.get('mark_as_un_read'):
+            return "UNREAD"
+        elif action.get('to_archive'):
+            return "ARCHIVE"
+        elif action.get('is_label'):
+            return "LABELS"
 
 
